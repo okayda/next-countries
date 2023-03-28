544566fb133eda48c5996c347e199dea963f7d5b
Jhon <59811681+okayda@users.noreply.github.com>
2023-03-28 11:40:40 -0700

    Add mobile & desktop layout for country details

--- components/CountryDetail/CountryDetail.js
+++ components/CountryDetail/CountryDetail.js
@@ -5,25 +5,42 @@ import ListOne from "./List/ListOne";
 import ListTwo from "./List/ListTwo";
 import BorderList from "./List/BorderList";
 
+import { BsGlobeAmericas } from "react-icons/bs";
+import { BiArrowBack } from "react-icons/bi";
+
 const CountryDetail = function () {
   return (
     <div className={classes.detail}>
-      <div className={classes.detail__img}>
-        <img src={flag} alt="Flag" />
-      </div>
+      <button className={classes.detail__btn}>
+        <BiArrowBack />
+        Back
+      </button>
 
-      <div className={classes.detail__content}>
-        <h1>United States</h1>
-
-        <div className={classes.detail__contents}>
-          <ListOne title={classes["detail__contents--title"]} />
-          <ListTwo title={classes["detail__contents--title"]} />
+      <div className={classes.detail__wrapper}>
+        <div className={classes.detail__flag}>
+          <img src={flag} alt="Flag" />
         </div>
-      </div>
 
-      <div className={classes.detail__borders}>
-        <h2>Border Countries:</h2>
-        <BorderList />
+        <div>
+          <div className={classes.detail__title}>
+            <h1>United States</h1>
+            <a href="#">
+              Location <BsGlobeAmericas />
+            </a>
+          </div>
+
+          <div className={classes["detail__list-wrapper"]}>
+            <div className={classes.detail__contents}>
+              <ListOne title={classes["detail__contents--title"]} />
+              <ListTwo title={classes["detail__contents--title"]} />
+            </div>
+
+            <div className={classes.detail__borders}>
+              <h2>Border Countries:</h2>
+              <BorderList />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
